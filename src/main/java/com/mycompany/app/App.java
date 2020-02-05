a7c845e5a9a7bf33bae0978b06c9a3d063f182ba
JwalaInfosys <60133847+JwalaInfosys@users.noreply.github.com>
2020-02-05 15:49:08 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,35 +1,7 @@
 package com.mycompany.app;
-import java.util.*;
 
-/**
- * Hello world!
- */
 public class App
 {
-
 	public static void main(String[] args){
-    String s2="James Gosling";
-		
-		for(int i=0;i<2;i++)
-		{
-			System.out.println("Who is the Inverntor of Java??");
-			
-			Scanner s=new Scanner(System.in);
-			
-			String s1=s.nextLine();
-			
-			if(s2.equalsIgnoreCase(s1))
-			{
-				System.out.println("Yes correct");
-				System.exit(0);
-			}
-			else{
-				
-				System.out.println("Try Again");
-			}
-			
-		}
-		System.out.println(s2);
-		}
-
+	System.out.println("Welcome to CICD Deployment");		
 }
